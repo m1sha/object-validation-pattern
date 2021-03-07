fdfe2ff61b548ffb0599f4213d651994cae9d97b
m1sha <m1sha@live.ru>
2021-03-07 14:55:20 +0700

    [master] queue tests

--- test/queue.test.ts
+++ test/queue.test.ts
@@ -16,3 +16,31 @@ test('queue bad', async () => {
   expect(r).toBeFalsy()
   expect(rule.message).toEqual('must be false')
 })
+
+test('queue reset', () => {
+  const queue = new ValidationQueue()
+  queue.enqueue(new RuleQueueItem('rule1', () => true, 'rule 1'))
+  queue.enqueue(new RuleQueueItem('rule2', () => true, 'rule 2'))
+
+  expect(queue.dequeue()).toBeDefined()
+  expect(queue.dequeue()).toBeDefined()
+  expect(queue.dequeue()).toBeUndefined()
+
+  queue.reset()
+  expect(queue.dequeue()).toBeDefined()
+  expect(queue.dequeue()).toBeDefined()
+  expect(queue.dequeue()).toBeUndefined()
+
+  expect(queue.dequeue()).toBeUndefined()
+})
+
+test('queue many cases on one rule', () => {
+  const queue = new ValidationQueue()
+  queue.enqueue(new RuleQueueItem('rule1', () => true, 'rule 1 case 1'))
+  queue.enqueue(new RuleQueueItem('rule1', () => true, 'rule 1 case 2'))
+  queue.enqueue(new RuleQueueItem('rule2', () => true, 'rule 2 case 1'))
+
+  expect(queue.dequeue().items.length).toEqual(2)
+  expect(queue.dequeue().items.length).toEqual(1)
+  expect(queue.dequeue()).toBeUndefined()
+})
